6a78271f14feda1304e01c0ee6d716ba1e031609
Alexander Meissner <alexander.meissner@rwth-aachen.de>
2021-12-03 09:28:49 +0100

    playlist prototype

---
+++ RestAPI/RestAPI/RestAPI/Controllers/PlaylistCOntroller.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using RestAPI.Utility;
+
+namespace RestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlaylistController : ControllerBase
+    {
+        const string MusicFilepath = @"W:\Music";
+
+        [HttpGet]
+        public ActionResult<List<string>> GetPlaylists()
+        {
+            Logger.Trace("PlaylistController::GetPlaylists");
+
+            try
+            {
+                return new List<string>(Directory.EnumerateDirectories(MusicFilepath).Select(x => Path.GetFileName(x)));
+            }
+            catch (Exception exception)
+            {
+                Logger.Exception(exception);
+                return NotFound();
+            }
+            
+        }
+    }
+}
