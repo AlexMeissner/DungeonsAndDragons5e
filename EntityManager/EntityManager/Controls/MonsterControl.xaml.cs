35b64d4c2e2524c0a5dfd3cd59ed73354701ca75
Alexander Meissner <alexander.meissner@rwth-aachen.de>
2021-12-17 18:00:56 +0100

    use monster view model

--- EntityManager/EntityManager/Controls/MonsterControl.xaml.cs
+++ EntityManager/EntityManager/Controls/MonsterControl.xaml.cs
@@ -1,48 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Windows.Controls;
-using System.Windows.Media.Imaging;
-using EntityManager.Data;
+﻿using System.Windows.Controls;
 
 namespace EntityManager
 {
     public partial class MonsterControl : UserControl
     {
+        public MonsterViewModel ViewModel { get; set; } = new MonsterViewModel();
+
         public MonsterControl()
         {
             InitializeComponent();
-
-            List<Monster> items = new();
-
-            for (int i = 0; i < 25; ++i)
-            {
-                Monster monster = new();
-                monster.GUID = Guid.NewGuid().ToString();
-                monster.Name = "Eule";
-                monster.Image = new BitmapImage(new Uri(@"W:\Projects\Coding\Dungeons and Dragons Tabletop\DungeonsAndDragonsTabletop\Resources\Images\LoginScreen.png"));
-                monster.Strength = 3;
-                monster.Dexterity = 13;
-                monster.Constitution = 8;
-                monster.Intelligence = 2;
-                monster.Wisdom = 12;
-                monster.Charisma = 7;
-                monster.Size = "sehr kleines Tier";
-                monster.Alignment = "gesinnungslos";
-                monster.ArmorClass = 11;
-                monster.HitInfo = new MonsterHitInfo() { HitPoints = 20, HitDice = "1W4 - 1" };
-                monster.Movement = new string[] { "1.5m", "fliegend 18m" };
-                monster.Senses = new string[] { "Dunkelsicht 36m", "passive Wahrnehmung 13" };
-                monster.Languages = new string[] { "Eulisch", "Allgemeinsprache" };
-                monster.Difficulty = new MonsterDifficulty() { ChallengeRating = 1.0 / 8.0, ExperiencePoints = 25 };
-                monster.Actions = new Data.Action[]
-                {
-                    new Data.Action(){Name = "Test 1", Type="Nahkampf-Waffenangriff", Description = "sdfguhs dsfiouh fsipdfh s ifsiufsdifh spdfs idfsi udfisudhfipsudhfisdhf  if sifu sidu fhsiudf s ipd gsiu sdiu sd" },
-                    new Data.Action(){Name = "Test 2", Description = "pdsui his hbsi spiu isadsiuv sdiu vpsidvipusd visdv iujspdviujpsd iuvpsdiu vsdiuv sdiuvsdiuvsdipuvbsdpi uv bsdpiu v" }
-                };
-                items.Add(monster);
-            }
-
-            ItemsCtr.ItemsSource = items;
         }
     }
 }
--- EntityManager/EntityManager/ViewModels/MonsterViewModel.cs
+++ EntityManager/EntityManager/ViewModels/MonsterViewModel.cs
@@ -1,9 +1,57 @@
-﻿using System.Windows.Media.Imaging;
+﻿using System;
+using System.Windows.Media.Imaging;
+
+using System.Windows.Input;
+using System.Collections.ObjectModel;
+using EntityManager.Windows;
+using EntityManager.Data;
 
 namespace EntityManager
 {
     public class MonsterViewModel : BaseViewModel
     {
-        public BitmapImage Image { get; set; } = new BitmapImage(new System.Uri(@"W:\Projects\Coding\Dungeons and Dragons Tabletop\DungeonsAndDragonsTabletop\Resources\Images\LoginScreen.png"));
+        public ObservableCollection<Monster> Monsters { get; set; }
+        public ICommand CreateMonsterCommand { get; set; }
+
+        public MonsterViewModel()
+        {
+            CreateMonsterCommand = new RelayCommand(OpenMonsterCreationWindow);
+
+            Monsters = new ObservableCollection<Monster>();
+
+            for (int i = 0; i < 27; ++i)
+            {
+                Monster monster = new();
+                monster.GUID = Guid.NewGuid().ToString();
+                monster.Name = "Eule";
+                monster.Image = new BitmapImage(new Uri(@"W:\Projects\Coding\Dungeons and Dragons Tabletop\DungeonsAndDragonsTabletop\Resources\Images\LoginScreen.png"));
+                monster.Strength = 3;
+                monster.Dexterity = 13;
+                monster.Constitution = 8;
+                monster.Intelligence = 2;
+                monster.Wisdom = 12;
+                monster.Charisma = 7;
+                monster.Size = "sehr kleines Tier";
+                monster.Alignment = "gesinnungslos";
+                monster.ArmorClass = 11;
+                monster.HitInfo = new MonsterHitInfo() { HitPoints = 20, HitDice = "1W4 - 1" };
+                monster.Movement = new string[] { "1.5m", "fliegend 18m" };
+                monster.Senses = new string[] { "Dunkelsicht 36m", "passive Wahrnehmung 13" };
+                monster.Languages = new string[] { "Eulisch", "Allgemeinsprache" };
+                monster.Difficulty = new MonsterDifficulty() { ChallengeRating = 1.0 / 8.0, ExperiencePoints = 25 };
+                monster.Actions = new Data.Action[]
+                {
+                    new Data.Action(){Name = "Test 1", Type="Nahkampf-Waffenangriff", Description = "sdfguhs dsfiouh fsipdfh s ifsiufsdifh spdfs idfsi udfisudhfipsudhfisdhf  if sifu sidu fhsiudf s ipd gsiu sdiu sd" },
+                    new Data.Action(){Name = "Test 2", Description = "pdsui his hbsi spiu isadsiuv sdiu vpsidvipusd visdv iujspdviujpsd iuvpsdiu vsdiuv sdiuvsdiuvsdipuvbsdpi uv bsdpiu v" }
+                };
+                Monsters.Add(monster);
+            }
+        }
+
+        public static void OpenMonsterCreationWindow()
+        {
+            MonsterCreationWindow window = new();
+            window.Show();
+        }
     }
 }
