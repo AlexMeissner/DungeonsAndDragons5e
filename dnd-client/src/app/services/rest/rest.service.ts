cf842e7eb74edcfb0282963639a49a2c49238772
Tim Thunig <tim.thunig@kisters.de>
2021-12-20 13:48:20 +0100

    Create first dto.

--- dnd-client/src/app/services/rest/rest.service.ts
+++ dnd-client/src/app/services/rest/rest.service.ts
@@ -1,16 +1,38 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from 'src/app/dto/user';
 
+//Mock examples
+let users: User[] = [
+  {
+    Username: 'Tim'
+  },
+  {
+    Username: 'Alex'
+  }
+]
 @Injectable({
   providedIn: 'root'
 })
+
 export class RestService
 {
 
-  private defOpt = {
-    observe: 'body',
-    responseType: 'json',
+  private options: object = {
+    responseType: "json",
     withCredentials: false
   };
 
-  constructor() { }
+  private baseUrl: string = 'localhost';
+
+  constructor(private http: HttpClient) { }
+
+  login(name: string, pw: string)
+  {
+    let body = {
+      username: name,
+      password: pw
+    };
+    return this.http.post<User>(this.baseUrl + '/login', body, this.options);
+  }
 }
