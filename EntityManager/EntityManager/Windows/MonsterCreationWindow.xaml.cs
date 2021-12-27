47880fa3865e9dd2578fb9070424a3f40e94acb5
Alexander Meissner <alexander.meissner@rwth-aachen.de>
2021-12-27 12:24:24 +0100

    monster image

--- EntityManager/EntityManager/Windows/MonsterCreationWindow.xaml.cs
+++ EntityManager/EntityManager/Windows/MonsterCreationWindow.xaml.cs
@@ -1,14 +1,16 @@
 ﻿using System;
+using System.IO;
 using System.Windows;
 using System.Windows.Input;
 using System.Text.RegularExpressions;
+using Microsoft.Win32;
 using EntityManager.ViewModels;
 
 namespace EntityManager.Windows
 {
     public partial class MonsterCreationWindow : Window
     {
-        public Monster? Monster { get; set; }
+        public Monster? Monster { get; set; } // TODO: Remove '?'
 
         public MonsterCreationWindow()
         {
@@ -43,6 +45,18 @@ namespace EntityManager.Windows
         {
             DialogResult = false;
             Close();
-        }       
+        }
+
+        private void OnChangeImage(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new();
+            dialog.Filter = "Image files (*.png;*.jpg;*.jpeg;*.bmp)|*.png;*.jpg;*.jpeg;*.bmp";
+
+            if (dialog.ShowDialog() == true && Monster != null)
+            {
+                byte[] imageData = File.ReadAllBytes(dialog.FileName);
+                Monster.Image = Convert.ToBase64String(imageData);
+            }
+        }
     }
 }
